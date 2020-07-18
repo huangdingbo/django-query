1ab4c9b29e5b01b8afa68d14f58494e24274aeee
huangdingbo <hdjVERj3b>
2020-07-18 22:15:00 +0800

    0.0.1

---
+++ django_query/LikeConditionBuilder.py
@@ -0,0 +1,19 @@
+from django_query.CommonExpressBuilder import CommonExpressBuilder
+from django_query.Handler import Handler
+from django_query.LikeCondition import LikeCondition
+
+
+class LikeConditionBuilder(CommonExpressBuilder):
+
+    def build(self, expression: LikeCondition, params={}):
+        operator = Handler.strToUpper(expression.getOperator())
+        column = expression.getColumn()
+        values = expression.getValue()
+        escape = expression.escapingReplacements()
+
+        if not values:
+            return "0=1"
+
+        self.addParams(values)
+
+        return column + " " + operator + " " + "%s"
